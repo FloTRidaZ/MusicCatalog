8a59e0716f9ed3f1a62d648f23609b8e616a3fa0
Karasev Sergey <karasevsergey479@gmail.com>
2020-10-23 01:17:21 +0300

    Added a track playing

--- Client01/TrackPage.xaml.cs
+++ Client01/TrackPage.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using Client01.Scripts;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -34,6 +38,31 @@ namespace Client01
             Track target = e.Parameter as Track;
             Cover.Source = target.Album.CoverSrc;
             TrackName.Text = target.Name;
+            MediaPlayer player = new MediaPlayer();
+            _mediaPlayerElement.SetMediaPlayer(player);
+            CreateMediaSource(target, player);
+            player.Play();
+            
+        }
+
+        private void CreateMediaSource(Track target, MediaPlayer player)
+        {
+            App app = Application.Current as App;
+            using(SqlConnection connection = new SqlConnection(app.GetConnectionString()))
+            {
+                connection.Open();
+                SqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT file_stream FROM music_table WHERE CAST(stream_id AS nvarchar(MAX)) LIKE '" + target.Asset.MusicSrcId.ToString() + "';";
+                using(SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    reader.Read();
+                    Stream stream = reader.GetStream(0);
+                    TrackName.Text = stream.Length.ToString();
+                    IRandomAccessStream randomStream = WindowsRuntimeStreamExtensions.AsRandomAccessStream(stream);
+                    MediaSource source = MediaSource.CreateFromStream(randomStream, "MPEG");
+                    player.Source = source;
+                }
+            }
         }
     }
 }
