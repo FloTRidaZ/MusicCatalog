74615bb47b8ad1915c1a37b2792cb37ce31c0728
Karasev Sergey <karasevsergey479@gmail.com>
2020-12-08 11:19:14 +0300

    final final final commit

--- Client01/ru/kso/Enum/PaneFooterType.cs
+++ Client01/ru/kso/Enum/PaneFooterType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Client01.ru.kso.Enum
+﻿namespace Client01.ru.kso.Enum
 {
     public enum PaneFooterType
     {
